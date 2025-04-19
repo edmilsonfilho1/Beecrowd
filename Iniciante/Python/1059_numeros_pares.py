5db0f331153fe756841c0b2124df2a724e41a0c3
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-19 19:10:58 -0300

    Create 1059_numeros_pares.py
    Adding resolution of exercise 1059

---
+++ Iniciante/Python/1059_numeros_pares.py
@@ -0,0 +1,6 @@
+#created a variable to use in the for range 
+#It will print n + 2 50 times, printing all the numbers necessary in this problem 
+n = 0 
+for i in range (50): 
+    n += 2
+    print(n)
