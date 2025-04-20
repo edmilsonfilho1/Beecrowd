980d02d079ea644205ebff5b3ae3337d22f3cb54
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-20 05:16:47 -0300

    Create 1067_numeros_impares.py
    Adding resolution of exercise 1067

---
+++ Iniciante/Python/1067_numeros_impares.py
@@ -0,0 +1,8 @@
+x = int(input())
+#Creates a variable with value 1 
+n = 1 
+#uses a while loop, so that n appears on the screen while it is less than or equal to x
+#And in each loop we add 2 to n so that it always shows odd numbers
+while n <= x: 
+    print(n)
+    n += 2 
