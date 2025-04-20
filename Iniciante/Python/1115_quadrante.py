afb6010d3f09dac686328ecd14a919ae0e4ebb6a
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-20 17:46:57 -0300

    Create 1115_quadrante.py
    Adding resolution of exercise 1115

---
+++ Iniciante/Python/1115_quadrante.py
@@ -0,0 +1,19 @@
+#Creates variables to use in the future 
+x = 1
+y = 1
+#Makes a boolean variable that verify if the loop should end  
+fim = x == 0 or y == 0 
+#Uses (fim) do start the loop
+while not fim: 
+    x, y = map(int, input().split())
+    #Makes conditionals that verify witch number to print based on math rules
+    if x > 0 and y > 0: 
+        print('Primeiro')
+    elif x < 0 and y > 0:
+        print('Segundo')
+    elif x < 0 and y < 0: 
+        print('Terceiro')
+    elif x > 0 and y < 0: 
+        print('Quarto')
+    else: 
+        break
