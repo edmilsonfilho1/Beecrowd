b2f45bca63f223da799783f71598b4ab481f7d45
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-20 11:52:03 -0300

    Create 1097_sequencia_ij3.py
    Adding resolution of exercise 1097

---
+++ Iniciante/Python/1097_sequencia_ij3.py
@@ -0,0 +1,26 @@
+i = 1 
+j = 7 
+#Create while loops to print the i and the j sequences 
+#Every loop we take 1 from j so the sequence get right
+while j >= 5: 
+    print(f'I={i} J={j}')
+    j += -1 
+j = 9
+while j >= 7: 
+     print(f'I={i+2} J={j}')
+     j += -1 
+j = 11 
+while j >= 9: 
+     print(f'I={i+4} J={j}')
+     j += -1
+j = 13 
+while j >= 11: 
+     print(f'I={i+6} J={j}')
+     j += -1
+j = 15 
+while j >= 13: 
+     print(f'I={i+8} J={j}')
+     j += -1
+         
+
+
