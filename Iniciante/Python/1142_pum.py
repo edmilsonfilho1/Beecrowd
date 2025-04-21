5a62e859ecdb4b76007ac2764be2916edc005cfd
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-21 20:00:48 -0300

    Create 1142_pum.py
    Adding resolution of exercise 1142

---
+++ Iniciante/Python/1142_pum.py
@@ -0,0 +1,11 @@
+n = int(input())
+#Create 3 variables with the inicial numbers needed
+num1 = 1 
+num2 = 2 
+num3 = 3 
+#Makes a for loop in (n) range that will print the numbers and increase 4 to them every time 
+for i in range (n): 
+    print(f'{num1} {num2} {num3} PUM')
+    num1 += 4 
+    num2 += 4 
+    num3 += 4
