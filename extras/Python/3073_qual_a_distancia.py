f5b3f41d75b51781ff16fb95ceccac0f080e071e
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-21 17:15:09 -0300

    Create 3073_qual_a_distancia.py
    Adding resolution of exercise 3073 (extra exercise)

---
+++ extras/Python/3073_qual_a_distancia.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+#Makes num1 the greatest one 
+if num1 < num2: 
+    num1, num2 = num2, num1
+if num1 < num3: 
+    num1, num3 = num3, num1
+#Makes num3 the smallest one 
+if num2 < num3: 
+    num2, num3 = num3, num2
+print(f'A distância entre o maior valor {num1} e o menor valor {num3} é {num1-num3}')
