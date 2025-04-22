e322f8b8974dfc84a3120f9153e9305f8c1b3b54
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-22 04:17:24 -0300

    Update 3073_qual_a_distancia.py
    Updating the resolution of exercise 3073 (no conditional methods allowed)

--- extras/Python/3073_qual_a_distancia.py
+++ extras/Python/3073_qual_a_distancia.py
@@ -1,13 +1,12 @@
 num1 = int(input())
 num2 = int(input())
 num3 = int(input())
-#Makes num1 the greatest one 
-if num1 < num2: 
-    num1, num2 = num2, num1
-if num1 < num3: 
-    num1, num3 = num3, num1
-#Makes num3 the smallest one 
-if num2 < num3: 
-    num2, num3 = num3, num2
+#Math formula to find out the greatest number
+maior_num1_num2 = (num1 + num2 + abs(num1-num2)) // 2 
+maior_geral = (maior_num1_num2 + num3 + abs(maior_num1_num2-num3)) // 2 
+#Math formula to find out the smallest number 
+menor_num1_num2 = (num1 + num2 - abs(num1-num2)) // 2 
+menor_geral = (menor_num1_num2 + num3 - abs(menor_num1_num2-num3)) // 2 
 #shows up the greatest and the smallest number and subtract them
-print(f'A distância entre o maior valor {num1} e o menor valor {num3} é {num1-num3}')
+print(f'A distância entre o maior valor {maior_geral} e o menor valor {menor_geral} é {maior_geral-menor_geral}')
+#Disclaimer: No conditional methods were allowed in this exercise 
