e92843284e24a5b7424f03f03f2efc732cd0b577
Edmilson Filho <89650955+edmilsonfilho1@users.noreply.github.com>
2025-04-22 20:13:58 -0300

    Update 1149_somando_inteiros_consecutivos.py
    Adding resolution of exercise 1149

--- Iniciante/Python/1149_somando_inteiros_consecutivos.py
+++ Iniciante/Python/1149_somando_inteiros_consecutivos.py
@@ -1,7 +1,21 @@
-a, n = map(int, input().split())
-while n <= 0: 
-    n = int(input())
-soma = 0 
-for i in range(n): 
-    soma += (a+i)
+valores = list(map(int, input().split()))
+
+A = valores[0]
+N = 0
+
+#It search the first n after a 
+for valor in valores[1:]:
+    if valor > 0:
+        N = valor
+        break
+
+#Loop to pick up the N input every time he is equal or under 0 
+while N <= 0:
+    N = int(input())
+
+#Add A + i until N-1
+soma = 0
+for i in range(N):
+    soma += A + i
+
 print(soma)
